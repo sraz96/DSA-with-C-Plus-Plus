e80929eb96ff3cdcab0972a1df1307043b2f476a
Santosh Maurya <121560172+sraz96@users.noreply.github.com>
2024-02-01 20:44:56 +0530

    Add files via upload

---
+++ 1Kanpsack.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int knapsack(int W, const vector<int>& weights, const vector<int>& values, int n) {
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (weights[i - 1] <= w) {
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+            } else {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+
+    return dp[n][W];
+}
+
+int main() {
+    vector<int> weights = {2, 3, 4, 5};
+    vector<int> values = {3, 4, 5, 6};
+    int n = weights.size();
+
+    int W = 5;
+
+    int maxValue = knapsack(W, weights, values, n);
+    cout << "Maximum value in the knapsack: " << maxValue << endl;
+
+    return 0;
+}
